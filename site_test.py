de2c800a8dd59c1c989b75141677106cf7895511
Ohad Amsellem <97289641+OhadAms@users.noreply.github.com>
2023-01-30 02:04:30 +0200

    Create site_test.py

---
+++ site_test.py
@@ -0,0 +1,14 @@
+import requests
+import sys
+
+URL = 'http://172.17.0.1:8081/jsp_file_for_final_project.jsp'
+
+
+def url_good(url)
+	r = requests.head(url)
+	if r.status_code == 200:
+		sys.exit(0)
+	else:
+		sys.exit(1)
+	
+url_good(URL)
