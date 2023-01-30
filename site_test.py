ddb9831e9c7833acda815a5d0d28a6b2d8e666d0
Ohad Amsellem <97289641+OhadAms@users.noreply.github.com>
2023-01-30 02:32:26 +0200

    Update site_test.py

--- site_test.py
+++ site_test.py
@@ -6,7 +6,7 @@ URL = 'http://172.17.0.1:8081/jsp_file_for_final_project.jsp'
 
 url_good(URL)
 
-def url_good(url)
+def url_good(url):
 	r = requests.head(url)
 	if r.status_code == 200:
 		sys.exit(0)
