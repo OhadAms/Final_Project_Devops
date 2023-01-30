aef1c4fe663f43bed3d6ba7041642e2f31574b2f
Ohad Amsellem <97289641+OhadAms@users.noreply.github.com>
2023-01-30 02:44:22 +0200

    Update site_test.py

--- site_test.py
+++ site_test.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 
+URL = 'http://172.17.0.1:8081/jsp_file_for_final_project.jsp'
 
 url_good(URL)
 
@@ -11,7 +12,6 @@ def url_good(url):
 	else:
 		sys.exit(1)
 
-URL = 'http://172.17.0.1:8081/jsp_file_for_final_project.jsp'
 
 
 
